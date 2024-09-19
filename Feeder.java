c2274858e74a75a30cd25491b3c4902fe4c53cde
rachelw106 <rachelw106@nycstudents.net>
2024-09-19 13:32:05 +0000

    commit

--- Feeder.java
+++ Feeder.java
@@ -11,7 +11,14 @@ public class Feeder {
      * Simulates one day with numBirds birds or possibly a bear at the bird
      * feeder, as described in part (a) Precondition: numBirds > 0
      */
-    public void simulateOneDay(int numBirds) {}
+    public void simulateOneDay(int numBirds) {
+        if(Math.random() < 0.05) currentFood = 0;
+        else {
+            int eaten = (int) (Math.random() * 41) + 10;
+            currentFood-= eaten * numBirds;
+            if(currentFood < 0) currentFood = 0;
+        }
+    }
 
     /**
      * Returns the number of days birds or a bear found food to eat at the
@@ -20,9 +27,11 @@ public class Feeder {
      */
     public int simulateManyDays(int numBirds, int numDays) 
     {
-        return 0;
+        int days = 0;
+        return days;
+
     }
-    
+
     public int getCurrentFood()
     {
         return currentFood;
@@ -34,4 +43,8 @@ public class Feeder {
     }
 
     public Feeder () {}
+
+    public String toString() {
+        return "Current Food: " + currentFood;
+    }
 }
--- Main.java
+++ Main.java
@@ -6,7 +6,22 @@ public class Main
         System.out.println(f);
         //System.out.println(f.currentFood);
         System.out.println(f.getCurrentFood());
+
         Feeder g = new Feeder(500);
         System.out.println(g.getCurrentFood());
+        System.out.println(Math.random());
+        double random = (int) (Math.random() * 40 + 10);
+        System.out.println(random);
+        System.out.println(Math.random() < 0.05);
+        g.simulateOneDay(12);
+        System.out.println(g.getCurrentFood());
+
+        Feeder h = new Feeder(1000);
+        h.simulateOneDay(22);
+        System.out.println(h.getCurrentFood());
+
+        Feeder i = new Feeder(100);
+        i.simulateOneDay(5);
+        System.out.println(i);
     }
 }
